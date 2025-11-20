8e818b97c5fbbbdae29fd6fb7256416888a88175
AUG__DOG <augustdagostino@icloud.com>
2025-11-20 18:12:31 +0000

    added  Advancements

--- src/main/java/aug/forgemaster/datagen/ModAdvancementProvider.java
+++ src/main/java/aug/forgemaster/datagen/ModAdvancementProvider.java
@@ -15,5 +15,48 @@ public class ModAdvancementProvider extends FabricAdvancementProvider {
 
     @Override
     public void generateAdvancement(RegistryWrapper.WrapperLookup wrapperLookup, Consumer<AdvancementEntry> consumer) {
+        AdvancementHolder getShard = Advancement.Builder.advancement()
+		.display(
+				ModBlocks.ATTACCA_SHARD, 
+				Text.literal("Shard of Flames"), 
+				Text.literal("Found a Shard of Attacca in a meteorite"), 
+				ResourceLocation.withDefaultNamespace("textures/gui/advancements/backgrounds/adventure.png"), // Background image for the tab in the advancements page, if this is a root advancement (has no parent)
+				AdvancementFrame.GOAL, 
+				true,
+				true, 
+				true 
+		)
+		.criterion("got_shard", InventoryChangedCriterion.Conditions.items(ModBlocks.ATTACCA_SHARD))
+		.build(consumer, Forgemaster.MOD_ID + "/get_shard");
+
+        AdvancementHolder getBrBlade = Advancement.Builder.advancement()
+        .parent(getShard)
+		.display(
+				ModItems.BROKEN_ATTACCA, 
+				Text.literal("Shattered Ferocity"),
+				Text.literal("Reforged half of the shattered blade, Attacca"), 
+				null, 
+				AdvancementFrame.GOAL, 
+				true, 
+				true, 
+				true 
+		)
+		.criterion("got_br_blade", InventoryChangedCriterion.Conditions.items(ModItems.BROKEN_ATTACCA))
+		.build(consumer, Forgemaster.MOD_ID + "/get_br_blade");
+
+        AdvancementHolder getBlade = Advancement.Builder.advancement()
+        .parent(getBrBlade)
+		.display(
+				ModItems.ATTACCA, 
+				Text.literal("Champion of The Forge"), 
+				Text.literal("Completed the shattered sword, Attacca"), 
+				null, 
+				AdvancementFrame.CHALLENGE, 
+				true, 
+				true,
+				true
+        )
+		.criterion("got_attacca", InventoryChangedCriterion.Conditions.items(ModItems.ATTACCA))
+		.build(consumer, Forgemaster.MOD_ID + "/get_attacca");
     }
 }
