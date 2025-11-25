677065eeecd7d74c99656ea98e91de6ccb31e2d5
BuildTools <unconfigured@null.spigotmc.org>
2025-11-24 19:40:47 -0600

    added

--- src/main/java/aug/forgemaster/block/entity/ModBlockEntities.java
+++ src/main/java/aug/forgemaster/block/entity/ModBlockEntities.java
@@ -11,7 +11,7 @@ import net.minecraft.util.Identifier;
 public class ModBlockEntities {
     public static final BlockEntityType<PedestalBlockEntity> PEDESTAL_BE =
             Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(Forgemaster.MOD_ID, "pedestal_be"),
-                    new FabricBlockEntityTypeBuilder((type, pos) -> new PedestalBlockEntity(pos, ), ModBlocks.PEDESTAL).build());
+                    BlockEntityType.Builder.create(PedestalBlockEntity::new, ModBlocks.PEDESTAL).build(null));
 
     public static void registerBlockEntities() {
         Forgemaster.LOGGER.info("Registering Block Entities for" + Forgemaster.MOD_ID);
